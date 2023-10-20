0974b4fecdbaf03c796aa13a79d0eac440c7274b
barnaszabi96 <barnaszabi96@gmail.com>
2023-10-20 20:31:02 +0200

    Implemented cart features and order features

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -37,6 +37,8 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -56,8 +58,13 @@ export default cartSlice.reducer;
 
 // These Redux selector fns may cause perf issues in large applications --> reselect library
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
